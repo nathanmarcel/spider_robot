55e608d2d9fc1e1928ec763c253b3f749c8bce55
nathanmarcel <93691232+nathanmarcel@users.noreply.github.com>
2023-08-17 13:23:12 -0700

    oh yeah
    created arduino mega mount, tested one servo motor

--- sg90_test/sg90_test.ino
+++ sg90_test/sg90_test.ino
@@ -5,17 +5,27 @@ Servo servo_9;
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
   servo_9.attach(9, 500, 2500);
 }
 
 void loop() {
   for (pos = 0; pos <= 180; pos += 1) {
     servo_9.write(pos);
+    if (pos % 10 == 0) {
+      Serial.println(pos);
+      }
     delay(15); 
   }
   for (pos = 180; pos >= 0; pos -= 1) {
     servo_9.write(pos);
+    if (pos % 10 == 0) {
+      Serial.println(pos);
+      }
     delay(15); 
   }
+
+  
+
 
 }
